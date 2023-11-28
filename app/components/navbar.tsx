4174ebac2b26c56e2f29b0fd1381e0d5763fc511
Sergio Daguil <daguilgio@gmail.com>
2023-11-28 20:59:31 +0800

    reset_changes_to_fix_image_only

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../../public/img/logo.png'
 
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-purple-300 p-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-4">
-          <div className="text-gray-800 font-bold text-lg"><img src="../../favicon.ico" alt="logo" /></div>
+          <div className="text-gray-800 font-bold text-lg"><img src={logo.src} /></div>
           <div className="hidden lg:flex space-x-4" id='links'>
             <a href="#" className="bg-purple-300 text-black font-semibold py-2 px-4 rounded-full hover:bg-purple-500 hover:text-white focus:outline-none focus:shadow-outline-blue active:bg-purple-800">
               Home
