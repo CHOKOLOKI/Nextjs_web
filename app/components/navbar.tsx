8ece55088f10739d035e4369666bedbf85c7c1a3
Sergio Daguil <daguilgio@gmail.com>
2023-12-01 22:08:07 +0800

    update color palette

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,36 +3,36 @@ import logo from '../../public/img/logo.png'
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-purple-300 p-4 shadow-md">
+    <nav className="p-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-4">
           <div className="text-gray-800 font-bold text-lg"><img src={logo.src} /></div>
           <div className="hidden lg:flex space-x-4" id='links'>
-            <a href="#" className="bg-purple-300 text-black font-semibold py-2 px-4 rounded-full hover:bg-purple-500 hover:text-white focus:outline-none focus:shadow-outline-blue active:bg-purple-800">
+            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
               Home
             </a>
-            <a href="#" className="bg-purple-300 text-black font-semibold py-2 px-4 rounded-full hover:bg-purple-500 hover:text-white focus:outline-none focus:shadow-outline-blue active:bg-purple-800">
+            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
               About
             </a>
-            <a href="#" className="bg-purple-300 text-black font-semibold py-2 px-4 rounded-full hover:bg-purple-500 hover:text-white focus:outline-none focus:shadow-outline-blue active:bg-purple-800">
+            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
               Projects
             </a>
-            <a href="#" className="bg-purple-300 text-black font-semibold py-2 px-4 rounded-full hover:bg-purple-500 hover:text-white focus:outline-none focus:shadow-outline-blue active:bg-purple-800">
+            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
               Contact
             </a>
-            <a href="#" className="bg-purple-300 text-black font-semibold py-2 px-4 rounded-full hover:bg-purple-500 hover:text-white focus:outline-none focus:shadow-outline-blue active:bg-purple-800">
+            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
               Hobbies
             </a>
           </div>
         </div>
-        <div className="flex items-center space-x-4">
+        {/* <div className="flex items-center space-x-4">
           <a href="#" className="text-gray-800 hover:text-blue-500">
             Sign In
           </a>
           <a href="#" className="text-gray-800 hover:text-blue-500">
             Settings
           </a>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
