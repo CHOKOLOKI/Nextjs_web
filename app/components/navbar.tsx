8973adc5565c98430753cb847d608822e6308eac
Sergio Daguil <daguilgio@gmail.com>
2023-12-12 20:43:21 +0800

    remove-not-used-lines

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,38 +1,27 @@
 import React from 'react';
-import logo from '../../public/img/logo.png'
+import Link from 'next/link'
+
 
 const Navbar: React.FC = () => {
   return (
     <nav className="p-4 shadow-md">
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container mx-8 flex items-center justify-between">
         <div className="flex items-center space-x-4">
-          <div className="text-gray-800 font-bold text-lg"><img src={logo.src} /></div>
-          <div className="hidden lg:flex space-x-4" id='links'>
-            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
+          <div className="hidden lg:flex space-x-4">
+            <Link href="/" className=" text-black font-bold py-2 px-4 rounded-full focus:outline-0 focus:shadow-outline-blue active:shadow-md" id='links'>
               Home
-            </a>
-            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
+            </Link>
+            <Link href="/" className=" text-black font-bold py-2 px-4 rounded-full focus:outline-0 focus:shadow-outline-blue active:shadow-md" id='links'>
               About
-            </a>
-            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
+            </Link>
+            <Link href="/" className=" text-black font-bold py-2 px-4 rounded-full focus:outline-0 focus:shadow-outline-blue active:shadow-md" id='links'>
               Projects
-            </a>
-            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
+            </Link>
+            <Link href="/" className=" text-black font-bold py-2 px-4 rounded-full focus:outline-0 focus:shadow-outline-blue active:shadow-md" id='links'>
               Contact
-            </a>
-            <a href="#" className=" text-black font-semibold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-orange-200" id='links'>
-              Hobbies
-            </a>
+            </Link>
           </div>
         </div>
-        {/* <div className="flex items-center space-x-4">
-          <a href="#" className="text-gray-800 hover:text-blue-500">
-            Sign In
-          </a>
-          <a href="#" className="text-gray-800 hover:text-blue-500">
-            Settings
-          </a>
-        </div> */}
       </div>
     </nav>
   );
