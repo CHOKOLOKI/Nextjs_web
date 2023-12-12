fd31383f11817b35d19f26853d0ab7e3a911e8c8
Sergio Daguil <daguilgio@gmail.com>
2023-12-12 20:44:53 +0800

    add unsaved

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Navbar: React.FC = () => {
   return (
     <nav className="p-4 shadow-md">
-      <div className="container mx-8 flex items-center justify-between">
+      <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-4">
           <div className="hidden lg:flex space-x-4">
             <Link href="/" className=" text-black font-bold py-2 px-4 rounded-full focus:outline-0 focus:shadow-outline-blue active:shadow-md" id='links'>
